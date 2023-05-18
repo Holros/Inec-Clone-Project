b8ec79a0348e267c0c13b7c6a78bdf13e3a6debd
Holros <olasco4real28@gmail.com>
2023-05-18 22:37:15 +0100

    fixed and completed the altbar

--- static/js/inec.js
+++ static/js/inec.js
@@ -1,15 +1,38 @@
-let dropdownContentAltbar = document.getElementById("dropdown_content_altbar")
-let altbarIcon = document.getElementById("menu_cancel") 
-let altbar = document.querySelector(".altbar")
-altbar.addEventListener("click",update)
-console.log("hi")
-function update() {
+{
+  let dropdownContentAltbar = document.getElementById(
+    "dropdown_content_altbar"
+  );
+  let altbarIcon = document.getElementById("menu_cancel");
+  let altbar = document.querySelector(".altbar");
+  altbar.querySelector(".altbar > div").addEventListener("click", update);
+  console.log("hi");
+  function update() {
     if (dropdownContentAltbar.style.display == "flex") {
-        dropdownContentAltbar.style.display = "none"
-        altbarIcon.className = "fas fa-bars"
+      dropdownContentAltbar.style.display = "none"; 
+      altbarIcon.className = "fas fa-bars";
+    } else {
+      dropdownContentAltbar.style.display = "flex";
+      altbarIcon.className = "fa-solid fa-xmark fa-beat-fade";
     }
-    else{
-        dropdownContentAltbar.style.display = "flex"
-        altbarIcon.className = "fa-solid fa-xmark fa-beat-fade"
-    }
+  }
 }
+
+{
+   let individualDropdown = document.querySelectorAll(".dropdown-two")
+   for (let i = 0; i < individualDropdown.length; i++) {
+    individualDropdown[i].querySelector(".dropdown-two > a").addEventListener("click",expand)
+    individualDropdown[i].querySelector(".dropdown-content-altbar-two").style.display = "none"
+    function expand(event) {
+      event.preventDefault()
+      if(individualDropdown[i].querySelector(".dropdown-content-altbar-two").style.display =="none") {
+        individualDropdown[i].querySelector(".dropdown-content-altbar-two").style.display = "flex" 
+        individualDropdown[i].querySelector(".dropdown-two i").style.rotate = "180deg"
+      }
+      else{
+        individualDropdown[i].querySelector(".dropdown-content-altbar-two").style.display = "none"
+        individualDropdown[i].querySelector(".dropdown-two i").style.rotate = "0deg"
+      }
+     }
+   }
+}
+
